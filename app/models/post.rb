b37efdd80d0d181bc8a7160f69cf1d8d19776441
nick <t0pth4t@yahoo.com>
2013-10-10 20:58:29 -0500

    more fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::ForbiddenAttributesProtection
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   attr_accessible :title, :text
