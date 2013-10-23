b9507b7ba6b8dba637070e67f00a8aeeff89035a
nick <t0pth4t@yahoo.com>
2013-10-22 21:28:25 -0500

    fixed horizontal scroll

--- app/controllers/users/password_controller.rb
+++ app/controllers/users/password_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::PasswordsController < Devise::PasswordsController
   def resource_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
   end
   private :resource_params
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   def resource_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation,:current_password)
   end
   private :resource_params
 end
