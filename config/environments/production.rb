b18318dc4084e6eb002d99d5962c8a633bc011bf
nick <t0pth4t@yahoo.com>
2013-10-27 12:25:06 -0500

    added cloud front

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Blog::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+   config.action_controller.asset_host = "http://d3tg9bu578nx8n.cloudfront.net/"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
