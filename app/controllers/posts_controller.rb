e40d89716d11b1cb1ebefeda13592eab217fb571
nick <t0pth4t@yahoo.com>
2013-10-28 20:07:43 -0500

    more terrible inline styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ load_and_authorize_resource
   def index
     if params[:tag]
       @posts = Post.paginate(:page=>params[:page], :per_page =>5).order('created_at DESC').tagged_with(params[:tag])
+      @tag = params[:tag]
     else
       @posts = Post.paginate(:page=>params[:page], :per_page =>5).order('created_at DESC')
     end
