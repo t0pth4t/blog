91c2eb0ce5d44b1ec6964a27247657cf43635ef3
nick <t0pth4t@yahoo.com>
2013-10-29 20:06:32 -0500

    sidebar nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,3 @@
 //= require mediaelement_rails
 //= require_tree .
 
-$('.dropdown-toggle').click(function(e) {
-    e.preventDefault();
-    setTimeout($.proxy(function() {
-        if ('ontouchstart' in document.documentElement) {
-            $(this).siblings('.dropdown-backdrop').off().remove();
-        }
-    }, this), 0);
-});
